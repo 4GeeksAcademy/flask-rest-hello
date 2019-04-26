4b8c7bfa66805d900a9e28418ebce88bfc986372
Alejandro Sanchez <aalejo@gmail.com>
2019-04-26 07:14:36 +0000

    not ready yet

--- src/main.py
+++ src/main.py
@@ -1,29 +1,38 @@
+import os
 from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_swagger import swagger
+from models import db, Person
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+db.init_app(app)
 
 
 @app.route('/')
 def hello_world():
-    return "Hello World!"
+    """
+    Read documentation
+    """
+    return jsonify(swagger(app))
 
 
 @app.route('/person', methods=['POST', 'GET'])
 def handle_person():
     if request.method == 'POST':
         """return the information for <user_id>"""
-        user1 = Person(username='alesanchezr', email='hello@gmail.com')
+        user1 = Person(username=request.data['username'], email='hello@gmail.com')
         db.session.add(user1)
         db.session.commit()
 
     if request.method == 'GET':
-        return 'Hello, World!'
+        all_people = Person.query.all()
+        all_people = list(map(lambda x: x.to_json(), all_people))
+        return jsonify(all_people)
+
+    return "Invalid Method", 404
 
 
 @app.route('/person/<int:person_id>', methods=['PUT', 'GET'])
@@ -34,3 +43,10 @@ def get_single_person(person_id):
     if request.method == 'GET':
         user1 = Person.query.get(person_id)
         return user1.to_json()
+
+    return "Invalid Method", 404
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 3000))
+    app.run(host='0.0.0.0', port=port)
--- src/models.py
+++ src/models.py
@@ -1,3 +1,7 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
 class Person(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
@@ -6,8 +10,8 @@ class Person(db.Model):
     def __repr__(self):
         return '<Person %r>' % self.username
 
-    def to_json(self):
-        return jsonify({
+    def to_dic(self):
+        return {
             "username": self.username,
             "email": self.email
-        })
+        }
