3e5f981a4150b38000ca9f405f2010dfad538b9b
Alejandro Sanchez <aalejo@gmail.com>
2019-04-27 09:57:07 +0000

    example

---
+++ welcome.py
@@ -0,0 +1,10 @@
+print("""
+
+WELCOME GEEK! 🐍 + 💻 = 🤓
+
+Run the following commands to start coding:
+
+- \033[94m$ pipenv run migrate\033[0m run database migrations (if pending)
+- \033[94m$ pipenv run start\033[0m start flask web server
+- \033[94m$ pipenv run diagram\033[0m create database diagram \n\n
+""")
