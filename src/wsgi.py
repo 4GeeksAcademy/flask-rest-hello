3f98506906099a2d66a866e780678a2108bead75
Alejandro Sanchez <aalejo@gmail.com>
2019-07-09 14:03:09 -0400

    Update wsgi.py

--- src/wsgi.py
+++ src/wsgi.py
@@ -1,3 +1,6 @@
+# This file was created to run the application on heroku using gunicorn.
+# Read more about it here: https://devcenter.heroku.com/articles/python-gunicorn
+
 from main import app as application
 
 if __name__ == "__main__":
