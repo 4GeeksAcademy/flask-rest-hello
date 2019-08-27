664b234d60226298a9774c1a58c10ade6d3d98af
Alejandro Sanchez <aalejo@gmail.com>
2019-08-27 18:39:36 +0000

    removing migrations

--- welcome.py
+++ welcome.py
@@ -6,7 +6,7 @@ WELCOME GEEK! 🐍 + 💻 = 🤓
 
 The server is already running, \033[94mctr + c\033[0m to stop the server if you like
 
-The following commands are available to stat coding:
+The following commands are available to run your code:
 
 - \033[94m$ pipenv run migrate\033[0m create database migrations (if models.py is edited)
 - \033[94m$ pipenv run upgrade\033[0m run database migrations (if pending)
