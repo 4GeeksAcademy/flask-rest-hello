b054018965ab66ee9c11c45398f0754c428ac4ef
ColbyWTaylor <colbyt.093@gmail.com>
2019-10-28 13:19:44 -0400

    typo

--- src/main.py
+++ src/main.py
@@ -37,7 +37,7 @@ def handle_person():
 
     return jsonify(response_body), 200
 
-# this only runs if `$ python src/main.py` is exercuted
+# this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=False)
