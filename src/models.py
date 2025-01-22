beca5135c67d456489b5d554b30a780ec479df3a
Rosinni Rodríguez <43686922+rosinni@users.noreply.github.com>
2025-01-22 17:11:39 +0000

    update boilerplate

--- src/models.py
+++ src/models.py
@@ -1,12 +1,17 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import mapped_column
+from sqlalchemy import Integer, String, Boolean
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    __tablename__ = "user"
+
+    id = mapped_column(Integer, primary_key=True)
+    email = mapped_column(String(120), nullable=False)
+    password = mapped_column(String(80))
+    is_active = mapped_column(Boolean)
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -17,3 +22,25 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+
+
+# from flask_sqlalchemy import SQLAlchemy
+
+# db = SQLAlchemy()
+
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     email = db.Column(db.String(120), unique=True, nullable=False)
+#     password = db.Column(db.String(80), unique=False, nullable=False)
+#     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+
+#     def __repr__(self):
+#         return '<User %r>' % self.username
+
+#     def serialize(self):
+#         return {
+#             "id": self.id,
+#             "email": self.email,
+#             # do not serialize the password, its a security breach
+#         }
