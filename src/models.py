672a8ba1fe12640b1516c17e45808f1ed44c6f28
Rosinni Rodríguez <43686922+rosinni@users.noreply.github.com>
2025-02-14 11:58:35 +0100

    Update models.py

--- src/models.py
+++ src/models.py
@@ -20,25 +20,3 @@ class User(db.Model):
             "is_active": self.is_active
             # do not serialize the password, its a security breach
         }
-
-
-
-# from flask_sqlalchemy import SQLAlchemy
-
-# db = SQLAlchemy()
-
-# class User(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     email = db.Column(db.String(120), unique=True, nullable=False)
-#     password = db.Column(db.String(80), unique=False, nullable=False)
-#     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-
-#     def __repr__(self):
-#         return '<User %r>' % self.username
-
-#     def serialize(self):
-#         return {
-#             "id": self.id,
-#             "email": self.email,
-#             # do not serialize the password, its a security breach
-#         }
