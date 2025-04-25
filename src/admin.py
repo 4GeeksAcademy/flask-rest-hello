840ad4f9e588aa38bbdcfe911bea46c2377d37b9
Carlos <191144185+Carlosygaby@users.noreply.github.com>
2025-04-25 09:31:07 +0000

    Pending changes exported from your codespace

--- src/admin.py
+++ src/admin.py
@@ -1,6 +1,6 @@
 import os
 from flask_admin import Admin
-from models import db, User
+from models import db, User,People,Planets,Favorite
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
@@ -11,6 +11,10 @@ def setup_admin(app):
     
     # Add your models here, for example this is how we add a the User model to the admin
     admin.add_view(ModelView(User, db.session))
+    admin.add_view(ModelView(People, db.session))
+    admin.add_view(ModelView(Planets, db.session))
+    admin.add_view(ModelView(Favorite, db.session))
+    
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
--- src/models.py
+++ src/models.py
@@ -1,19 +1,82 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, ForeignKey, Enum
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from enum import Enum as pyEnum
 
 db = SQLAlchemy()
 
+
+# class FavoritesEnum(pyEnum):
+#     PLANETS = "planets"
+#     PEOPLE = "people"
+
+
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(
+        String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
-
+    favorites = relationship(
+        'Favorite', back_populates='user')
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+
+
+class People(db.Model):
+    __tablename__ = "people"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(30), nullable=False, unique=True)
+    favorites = relationship(
+        "Favorite", back_populates="people")
+
+    def serialize_people(self):
+        return {
+            "id": self.id,
+            "name": self.name
+        }
+
+
+class Planets(db.Model):
+    __tablename__ = "planets"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(30), nullable=False, unique=True)
+    favorites: Mapped[list['Favorite']] = relationship(
+        "Favorite", back_populates="planet")
+
+    def serialize_planets(self):
+        return {
+            "id": self.id,
+            "name": self.name
+        }
+
+
+class Favorite(db.Model):
+    __tablename__ = "favorites"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey('user.id'), nullable=False)
+    # favorite: Mapped[FavoritesEnum] = mapped_column(
+    #     Enum(FavoritesEnum, name="favorites_enum"))
+    planet_id: Mapped[int] = mapped_column(
+        ForeignKey('planets.id'), nullable=True)
+    people_id: Mapped[int] = mapped_column(ForeignKey('people.id'),nullable=True)
+    user = relationship(
+        "User", back_populates="favorites")
+    planet = relationship(
+        "Planets", back_populates="favorites")
+    people = relationship(
+        "People", back_populates="favorites")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            # "favorite": self.favorite.value,
+            "planet_id": self.planet_id,
+            "people_id": self.people_id
         }
